b39f4c54f9430fee1dd23ba0df027980604b44f1
KimlyBiss <denarnim@mail.ru>
2026-01-26 16:42:25 +0300

    new animation

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)

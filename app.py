f06bf9a46abbf06f104897328dc5a1244f81f373
KimlyBiss <denarnim@mail.ru>
2026-01-29 13:32:00 +0300

    new page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/methodology")
+def methodology():
+    return render_template("methodology.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
